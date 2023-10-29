2a39c099cb8f156b58906581afd95c83b9337bb9
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-10-29 20:05:18 +0700

    Web Updated

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className=''>Royal Indian Restaurant</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Layout/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            This is the Indian Royals Home page.
+        </div>
+    );
+};
+
+export default Home;
