e6bb8497182901693568f5108097dbf1e466ae33
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-11-09 21:23:43 +0700

    Setup a private route for testing purpuse

---
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useContext(AuthContext());
+    if (user) {
+        return children;
+    }
+    return (<Navigate to="/signin"></Navigate>);
+};
+
+export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,8 @@ import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/SignUp/Signup";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import Special from "../Pages/Special/Special";
 
 export const router = createBrowserRouter([
     {
@@ -32,6 +34,10 @@ export const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <Signup></Signup>
+            },
+            {
+                path: '/Offer',
+                element: <PrivateRoute> <Special></Special> </PrivateRoute>
             }
         ]
     },
