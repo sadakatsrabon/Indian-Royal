63c858b1974e0a29ce9f98072d73fe77837f2ef2
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-11-14 21:45:29 +0700

    Now possible to make an useer to an admin

--- src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
@@ -1,4 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const { data: users = [], isLoading, isError, refetch } = useQuery({
@@ -20,9 +23,70 @@ const AllUsers = () => {
         return <div>Error fetching data</div>;
     }
 
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is added to Admin!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    };
+
+    const handleDelete = (user) => {
+
+    };
+
     return (
-        <div>
-            {users.length}
+        <div className="w-full">
+            <Helmet>
+                Indian Royal || All users
+            </Helmet>
+            <h3 className="text-3xl font-semibold">Total Users : {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row */}
+                        {
+                            users.map((user, index) =>
+                                <tr key={user._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{
+                                        user.role === 'admin' ? 'Admin' :
+                                            <button onClick={() => handleMakeAdmin(user)} className="btn btn-outline btn-circle btn-sm"><FaUserShield /></button>
+
+                                    }</td>
+                                    <td>
+                                        <button onClick={() => handleDelete(user)} className="btn btn-outline bg-red-400 btn-sm">X</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
