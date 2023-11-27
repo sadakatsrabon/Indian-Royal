596c6153a1dec61f92b31e91ba4dcbe7e744947d
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-11-27 20:37:31 +0700

    Setup Payment's Primary level

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,6 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckOutForm from "./CheckOutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+// Provide publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_paymentGetway_PK);
 const Payment = () => {
     return (
         <div>
@@ -9,6 +14,9 @@ const Payment = () => {
                 heading="Pay now"
             ></SectionTitle>
             <h2>Payment System Page</h2>
+            <Elements stripe={stripePromise}>
+                <CheckOutForm></CheckOutForm>
+            </Elements>
         </div>
     );
 };
