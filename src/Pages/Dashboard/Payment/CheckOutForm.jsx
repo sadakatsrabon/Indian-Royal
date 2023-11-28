f6afebd1f8c0e8a092ef1d4feeed8088a108abd2
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-11-28 08:42:13 +0700

    Payment intent for Stripe is on the way

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,47 +1,63 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ price }) => {
     const stripe = useStripe();
     const elements = useElements();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (!stripe || elements) {
+        if (!stripe || !elements) {
+            console.log('error-1')
             return
         }
 
-        const card = elements.getElement(CardElement)
+        const card = elements.getElement(CardElement);
         if (card === null) {
-            return;
+            console.log('error-2')
+            return
+        }
+        console.log('card', card)
+        // Check Cart velidity
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            console.error('error', error)
+        }
+        else {
+            console.log('Payment Method', paymentMethod)
         }
 
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
+        <form className="w-2/3 mx-auto" onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
                             },
                         },
-                    }}
-                />
-                <button type="submit" disabled={!stripe}>
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <div className="divider"></div>
+            <div className="text-center mt-2">
+                <button className="btn btn-outline btn-sm w-28 bg-yellow-400" type="submit" disabled={!stripe}>
                     Pay
                 </button>
-            </form>
-        </div>
+            </div>
+        </form>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -3,19 +3,26 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import CheckOutForm from "./CheckOutForm";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hoocks/useCart";
 
 // Provide publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_paymentGetway_PK);
 const Payment = () => {
+
+    const [cart] = useCart();
+    // reduce mechanism
+    const total = cart.reduce((sum, item) => sum + item.price, 0)
+    const price = parseFloat(total.toFixed(2))
+
     return (
         <div>
             <Helmet> <title>Indian Royal || Payment</title></Helmet>
             <SectionTitle
                 heading="Pay now"
             ></SectionTitle>
-            <h2>Payment System Page</h2>
+            <h2 className="text-center mb-6yyyyy">Please be sure after payment. I will not be returnable</h2>
             <Elements stripe={stripePromise}>
-                <CheckOutForm></CheckOutForm>
+                <CheckOutForm price={price}></CheckOutForm>
             </Elements>
         </div>
     );
