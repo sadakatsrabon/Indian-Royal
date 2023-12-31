d454772cd63d9462ad3b0741113ec4726326f0ae
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-12-31 17:02:57 +0700

    Update Food menu Layout

--- src/Shared/MenuItem/MenuItem.jsx
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -2,15 +2,17 @@
 const MenuItem = ({ item }) => {
     const { image, price, recipe, name } = item;
     return (
-        <div className="flex space-x-4 px-8">
-            <img className="w-[120px] h-[10
-                0px] rounded-2xl" src={image} alt="" />
-            <div>
-                <h3 className="text-xl text-slate-500">{name}</h3>
-                <p className="">{recipe}</p>
+        <div className="border-2">
+            <div className="flex flex-row-reverse justify-between space-x-4 px-8">
+                <img className="w-[120px] h-[10
+                0px]" src={image} alt="" />
+                <div>
+                    <h3 className="text-xl text-slate-500">{name}</h3>
+                    <p className="">{recipe}</p>
+                </div>
             </div>
-            <div className="text-yellow-900 text-xl border-y-2 border-e-2 mt-2">
-                <p className=" p-5">${price}</p>
+            <div className="text-yellow-900 text-xl ml-8">
+                <p className=" px-5">${price}</p>
             </div>
         </div>
     );
