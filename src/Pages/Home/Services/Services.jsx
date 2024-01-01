c011098be3bd73ce1c180ce1e4a678d639f325fc
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-01-01 19:02:11 +0700

    added Service description

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -13,25 +13,29 @@ const Services = () => {
                     <div className="font-bold relative">
                         <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><TbBrandBooking /></div>
                     </div>
-                     <h4 className="uppercase font-semibold text-xl">hotel booking</h4>
+                    <h4 className="uppercase font-semibold text-xl">hotel booking</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
                         <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><MdOutlineNoFood /></div>
                     </div>
-                     <h4 className="uppercase font-semibold text-xl">Fresh Dishrs</h4>
+                    <h4 className="uppercase font-semibold text-xl">Fresh Dishrs</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
                         <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><IoFastFoodOutline /></div>
                     </div>
-                     <h4 className="uppercase font-semibold text-xl">halal food</h4>
+                    <h4 className="uppercase font-semibold text-xl">halal food</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
                         <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><TbTruckDelivery /></div>
                     </div>
-                     <h4 className="uppercase font-semibold text-xl">first delivery</h4>
+                    <h4 className="uppercase font-semibold text-xl">first delivery</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
                 </div>
             </div>
         </div>
