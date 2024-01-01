4572e9e1946184a04d3ab76b468e8f4571ec01d3
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-01 20:02:58 +0700

    Update Description

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -8,36 +8,40 @@ const Services = () => {
     return (
         <div className=" sm:flex mt-32 justify-around">
             <div className="w-30">
-                <img className=" my-auto" src={chef} alt="" />
+                <img className="p-4 m-4" src={chef} alt="" />
             </div>
-            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2 w-[100%]">
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
-                        <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><TbBrandBooking /></div>
+                        <div className="absolute  mt-[-30px] z-10 text-4xl p-2 text-yellow-500 bg-gray-700 rounded"><TbBrandBooking /></div>
                     </div>
-                    <h4 className="uppercase font-semibold text-xl">hotel booking</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
+                    <h4 className="uppercase font-semibold text-xl p-4 mt-4">hotel booking</h4>
+                    <div className="divider"></div>
+                    <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque .</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
-                        <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><MdOutlineNoFood /></div>
+                        <div className="absolute  mt-[-30px] z-10 text-4xl p-2 text-yellow-500 bg-gray-700 rounded"><MdOutlineNoFood /></div>
                     </div>
-                    <h4 className="uppercase font-semibold text-xl">Fresh Dishrs</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
+                    <h4 className="uppercase font-semibold text-xl p-4 mt-4">Fresh Dishrs</h4>
+
+<div className="divider"></div>                    <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque .</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
-                        <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><IoFastFoodOutline /></div>
+                        <div className="absolute  mt-[-30px] z-10 text-4xl p-2 text-yellow-500 bg-gray-700 rounded"><IoFastFoodOutline /></div>
                     </div>
-                    <h4 className="uppercase font-semibold text-xl">halal food</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
+                    <h4 className="uppercase font-semibold text-xl p-4 mt-4">halal food</h4>
+  
+  <div className="divider"></div>                  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque .</p>
                 </div>
                 <div className="border border-black p-4 m-8">
                     <div className="font-bold relative">
-                        <div className="absolute  mt-[-30px] z-10 text-4xl text-yellow-500 bg-gray-700 rounded"><TbTruckDelivery /></div>
+                        <div className="absolute  mt-[-30px] z-10 text-4xl p-2 text-yellow-500 bg-gray-700 rounded"><TbTruckDelivery /></div>
                     </div>
-                    <h4 className="uppercase font-semibold text-xl">first delivery</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque illo expedita laudantium nisi? Ipsam est corporis soluta cumque corrupti a rerum numquam, perspiciatis reprehenderit itaque distinctio quasi, aut possimus ea? Itaque.</p>
+                    <h4 className="uppercase font-semibold text-xl p-4 mt-4">first delivery</h4
+                    <div className="divider"></div>>
+                    <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident quam, blanditiis recusandae distinctio perferendis obcaecati illum facere cupiditate excepturi minima iusto voluptas esse expedita libero cumque? Cumque .</p>
                 </div>
             </div>
         </div>
