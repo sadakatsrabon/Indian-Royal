59378a5cea4f65c58fc7db33b5fa265ccfd9f1a2
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-03 11:31:16 +0700

    Home page ok for primary level

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -15,7 +15,7 @@ const Featured = () => {
                 </div>
                 <div className='md:ml-10'>
                     <p className='text-lime-300'>Are you looking for a luxury hotel room in resonable price ?</p>
-                    <p className='uppercase'>where can i get a rest house?</p>
+                    <p className='uppercase text-lg text-yellow-400 bg-slate-400 bg-opacity-30 rounded-lg p-2'>where can i get a rest house?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi distinctio, labore illo nostrum accusantium voluptatem deserunt? Autem soluta ad necessitatibus similique est corporis quisquam </p>
                     <Link to='/book'>
                         <button className="btn btn-outline btn-info border-0 border-y-4 bg-black bg-opacity-40 mt-4">Book Now</button></Link>
