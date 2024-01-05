e2b4e196d6be41e5fc19b4293317886789a142e4
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-06 06:45:12 +0700

    change the logo img to bg img

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -3,23 +3,25 @@ import logo from '../../assets/logo.png'
 
 const ContactUs = () => {
     return (
-        <div>
-            <div>
-                <Branch></Branch>
-            </div>
-            <div className="pt-36 p-20 flex w-full justify-center">
-                <img src={logo} alt="" />
-                <div className="flex flex-col gap-10">
-                    <input type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Your message" className="input input-bordered w-full max-w-xs" />
-                    <button className="btn btn-outline border-0 border-y-4 bg-slate-300">Send Message</button>
+        <div className="bg-slate-200">
+            <img className="fixed opacity-30" src={logo} alt="" />
+            <div className="z-10">
+                <div>
+                    <Branch></Branch>
                 </div>
-                <div className='bg-blue-800 text-white p-10 rounded-lg'>
-                    <p className="text-xl"><span className="p-2">Address : </span>Phnom Penh, Cambodia</p>
-                    <p className="text-xl"><span className="p-2">Phone : </span>+885 026365141</p>
-                    <p className="text-xl"><span className="p-2">Whats App : </span>+885 026365141</p>
-                    <p className="text-xl"><span className="p-2">Telegram : </span>+885 026365141</p>
-                    <p className="text-xl"><span className="p-2">Email : </span>indian@royal.com</p>
+                <div className="pt-36 p-20 flex w-full justify-center relative">
+                    <div className="flex flex-col gap-10">
+                        <input type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Your message" className="input input-bordered w-full max-w-xs" />
+                        <button className="btn btn-outline border-0 border-y-4 bg-slate-300">Send Message</button>
+                    </div>
+                    <div className='bg-blue-800 text-white p-10 rounded-lg'>
+                        <p className="text-xl"><span className="p-2">Address : </span>Phnom Penh, Cambodia</p>
+                        <p className="text-xl"><span className="p-2">Phone : </span>+885 026365141</p>
+                        <p className="text-xl"><span className="p-2">Whats App : </span>+885 026365141</p>
+                        <p className="text-xl"><span className="p-2">Telegram : </span>+885 026365141</p>
+                        <p className="text-xl"><span className="p-2">Email : </span>indian@royal.com</p>
+                    </div>
                 </div>
             </div>
         </div>
