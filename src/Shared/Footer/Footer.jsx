2714387f4b52e134cb41aebc8eda125f77422442
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-06 06:25:17 +0700

    Contact Button Style updated

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
                         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
                     </div>
                     <h6>IndianRoyal@gmail.com</h6>
-                    <button><Link to="/contact">Contact Us</Link> </button>
+                    <button className='mx-auto btn'><Link to="/contact">Contact Us</Link> </button>
                 </nav>
                 <div className='m-auto'>
                     <h3 className='text-2xl text-yellow-400 text-center mx-auto'>Tipu Sultan</h3>
