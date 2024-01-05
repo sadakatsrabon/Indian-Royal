888fd2fd2e35865e81beea65c7553821c7ee6587
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-05 21:11:53 +0700

    Change the price style for user experience

--- src/Shared/MenuItem/MenuItem.jsx
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -4,15 +4,15 @@ const MenuItem = ({ item }) => {
     return (
         <div className="border border-black rounded-lg">
             <div className="flex flex-row-reverse justify-between space-x-4 px-8">
-                <img className="w-[120px] h-[10
-                0px] py-auto pt-4" src={image} alt="" />
+                <img className="w-[120px] h-[100px] h-[10
+                0px] my-auto pt-4" src={image} alt="" />
                 <div>
                     <h3 className="text-xl text-slate-500">{name}</h3>
                     <p className="">{recipe}</p>
                 </div>
             </div>
             <div className="text-pink-700 font-semibold text-xl ml-8">
-                <p className=" px-5">${price}</p>
+                <p className=" px-5 pb-1">${price}</p>
             </div>
         </div>
     );
