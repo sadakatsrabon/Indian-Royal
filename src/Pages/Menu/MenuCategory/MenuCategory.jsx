cecc1a1dad4fd7cba31be716caf801d6aabd9e39
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-05 20:53:56 +0700

    Change the menu size

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -7,7 +7,7 @@ const MenuCategory = ({ items, title, img }) => {
     return (
         <div className="pt-8">
             {title && <Cover bgImage={img} title={title}></Cover>}
-            <div className="grid md:grid-cols-2 gap-4 my-16">
+            <div className="grid md:grid-cols-2 gap-4 my-16 w-[90%]">
                 {
                     items.map((item) => <MenuItem
                         key={item._id}
