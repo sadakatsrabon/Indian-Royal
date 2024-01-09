ee71c30f57ecea0ec360f00b52d30967a062d028
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-09 11:45:41 +0700

    Added Branch

--- src/Pages/ContactUs/Branch/Branch.jsx
+++ src/Pages/ContactUs/Branch/Branch.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Branch = () => {
     return (
         <div>
+            <div>
+                <div>Our Branch</div>
+                <div>Our Branch</div>
+                <div>Our Branch</div>
+            </div>
             <h2 className='text-center mt-10 text-xl'>We have 3 branches across Cambodia. Our branch will show here</h2>
         </div>
     );
