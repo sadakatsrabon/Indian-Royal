1a32398e9d5b73379ed79b98d2b166fffa1b2b98
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-10 06:41:00 +0700

    Added secound branch name

--- src/Pages/ContactUs/Branch/Branch.jsx
+++ src/Pages/ContactUs/Branch/Branch.jsx
@@ -5,7 +5,7 @@ const Branch = () => {
         <div>
             <div>
                 <div>Phonm Penh Branch</div>
-                <div>Our Branch</div>
+                <div>Sianovill Branch</div>
                 <div>Our Branch</div>
             </div>
             <h2 className='text-center mt-10 text-xl'>We have 3 branches across Cambodia. Our branch will show here</h2>
