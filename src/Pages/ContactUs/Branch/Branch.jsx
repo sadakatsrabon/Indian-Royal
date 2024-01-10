11e857a324bcabd05bcae72cd7514d1c768f8117
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-10 11:22:28 +0700

    Added third brunch

--- src/Pages/ContactUs/Branch/Branch.jsx
+++ src/Pages/ContactUs/Branch/Branch.jsx
@@ -4,9 +4,10 @@ const Branch = () => {
     return (
         <div>
             <div>
+                
                 <div>Phonm Penh Branch</div>
                 <div>Sianovill Branch</div>
-                <div>Our Branch</div>
+                <div>Siam reap Branch</div>
             </div>
             <h2 className='text-center mt-10 text-xl'>We have 3 branches across Cambodia. Our branch will show here</h2>
         </div>
