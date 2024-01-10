263173f4e4af64d381f3ec2580cc56da80b00878
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-11 06:32:48 +0700

    Added Branch card on Branch section

--- src/Pages/ContactUs/Branch/Branch.jsx
+++ src/Pages/ContactUs/Branch/Branch.jsx
@@ -3,7 +3,36 @@ import React from 'react';
 const Branch = () => {
     return (
         <div>
-            <h2 className='text-center mt-10 text-xl'>We have 3 branches across Cambodia. Our branch will show here</h2>
+            <div><div className="card w-96 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div></div>
+            <div><div className="card w-96 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div></div>
+            <div><div className="card w-96 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div></div>
         </div>
     );
 };
