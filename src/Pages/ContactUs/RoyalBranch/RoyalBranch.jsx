12973953a3ce50077c5c185734b66668928c1b46
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-12 21:27:07 +0700

    Update barnch

--- src/Pages/ContactUs/RoyalBranch/RoyalBranch.jsx
+++ src/Pages/ContactUs/RoyalBranch/RoyalBranch.jsx
@@ -5,24 +5,24 @@ const RoyalBranch = () => {
   return (
     <div className="flex">
       {/* Sihanoukville */}
-      <div>
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <div className="pt-20 mx-auto">
+        <div className="card w-96 h-52 bg-base-100 shadow-xl image-full">
           <figure><img src={branchImg1} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">Sihanoukville</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-              z <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary">Buy Now</button>
             </div>
           </div>
         </div>
       </div>
       {/* Phom Penh */}
-      <div>
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <div className="pt-20 mx-auto">
+        <div className="card w-96 h-52 bg-base-100 shadow-xl image-full">
           <figure><img src={branchImg2} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">Phom Penh</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
@@ -31,11 +31,11 @@ const RoyalBranch = () => {
         </div>
       </div>
       {/* Siem Reap */}
-      <div>
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <div className="pt-20 mx-auto">
+        <div className="card w-96 h-52 bg-base-100 shadow-xl image-full">
           <figure><img src={branchImg3} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">Siem Reap</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
