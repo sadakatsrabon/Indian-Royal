2e023f34f41feb1308f3b7c80674e16a5c212dc2
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-12 22:23:44 +0700

    Added Category Image

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -16,30 +16,92 @@ import slider2 from "../../../assets/home/slide2.jpg";
 import slider3 from "../../../assets/home/slide3.jpg";
 import slider4 from "../../../assets/home/slide4.jpg";
 import slider5 from "../../../assets/home/slide5.jpg";
-import slider6 from "../../../assets/menu/banner3.jpg";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
     <section>
       <>
+        <SectionTitle
+          heading={"Our Food Categories"}
+        ></SectionTitle>
+        <div className='flex justify-around'>
+          <div>
+<h2 className='text-6xl font-bold'>Hi sir,</h2>
+<h2>You can see our royal category</h2>
+          </div>
+          <div className='w-72'>
+            <Swiper
+              effect={'cards'}
+              grabCursor={true}
+              modules={[EffectCards]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider1} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider2} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider3} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider4} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider5} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider1} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider2} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider3} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider4} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2 className='text-xl font-bold'>Category</h2>
+                  <img className="w-72 h-full" src={slider5} alt="Slide Image" />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            
+          </div>
+        </div>
       </>
-      <SectionTitle
-        heading={"Our Food Categories"}
-      ></SectionTitle>
-      <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img className="w-72 h-full" src={slider1} alt="Slide Image" /></SwiperSlide>
-        <SwiperSlide><img className="w-72 h-full" src={slider2} alt="Slide Image" /></SwiperSlide>
-        <SwiperSlide><img className="w-72 h-full" src={slider3} alt="Slide Image" /></SwiperSlide>
-        <SwiperSlide><img className="w-72 h-full" src={slider4} alt="Slide Image" /></SwiperSlide>
-        <SwiperSlide><img className="w-72 h-full" src={slider5} alt="Slide Image" /></SwiperSlide>
-        <SwiperSlide><img className="w-72 h-full" src={slider6} alt="Slide Image" /></SwiperSlide>
-      </Swiper>
       {/* <div ref={sliderRef} className="keen-slider h-96 mb-12">
         <div className="keen-slider__slide number-slide1">
           <h3 className="text-3xl uppercase fixed bottom-1 bg-black opacity-50 ">Drinks</h3>
